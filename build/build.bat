572947083e05d370d74f334b9b4f2ae0f4dea682
Wiz <the_wizard_who_says_ni@hotmail.com>
2014-06-04 00:15:02 +0200

    Sound driver now builds live from its own disasm!

--- build/build.bat
+++ build/build.bat
@@ -1,7 +1,10 @@
 echo off
 cls
 echo Start of assembly
-cd ../disasm/
+cd ../disasm/sound/driver/
+..\..\..\tools\asw\asw.exe ..\..\cube.asm
+..\..\..\tools\asw\p2bin.exe ..\..\cube.p .\cube.bin -k -r $0000-$1fff
+cd ../../
 echo Executing assembler ...
 @"../tools/asm68k" /o ae- /p sf2.asm, "../build/sf2build-%date:~6,4%%date:~3,2%%date:~0,2%-%time:~0,2%%time:~3,2%%time:~6,2%.bin" > ../build/output.log
 echo End of assembly, produced sf2build-%date:~6,4%%date:~3,2%%date:~0,2%-%time:~0,2%%time:~3,2%%time:~6,2%.bin
