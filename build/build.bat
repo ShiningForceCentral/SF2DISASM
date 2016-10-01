9ad16ee1dcbc3b0010ef87d22fa143233e5da470
wizardwhosaysni <the_wizard_who_says_ni@hotmail.com>
2016-10-02 01:17:48 +0200

    Updated build.bat to be locale-agnostic (thanks BigNailCow !)

--- build/build.bat
+++ build/build.bat
@@ -1,7 +1,12 @@
 echo off
 cls
-
-
+for /f %%x in ('wmic path win32_localtime get /format:list ^| findstr "="') do set %%x
+set today=%Year%%Month%%Day%
+set hour=%Hour%
+set hour=0%hour%
+set hour=%hour:~-2%
+set minutes=%Minute%
+set seconds=%Second%
 echo -------------------------------------------------------------
 echo Start of assembly
 echo Assembling sound driver ...
@@ -10,7 +15,7 @@ cd ../disasm/sound/
 ..\..\tools\asw\p2bin.exe ..\sounddriver.p .\sounddriver.bin -k -r $0000-$1fff
 cd ../
 echo Assembling game ...
-SET "buildname=sf2build-%date:~6,4%%date:~3,2%%date:~0,2%-%time:~0,2%%time:~3,2%%time:~6,2%.bin"
+SET "buildname=sf2build-%today%-%hour%%minutes%%seconds%.bin"
 @"../tools/asm68k" /o ae- /p sf2.asm, "../build/%buildname%" > ../build/output.log
 echo End of assembly, produced %buildname%
 
