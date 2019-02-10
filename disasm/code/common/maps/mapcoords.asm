bce7094a3972280eee05e24cad94fad26d7f5a48
Wiz <the_wizard_who_says_ni@hotmail.com>
2019-02-10 23:11:36 +0100

    Removing useless asm file after map coords formatting

--- disasm/code/common/maps/mapcoords.asm
+++
@@ -1,7 +0,0 @@
-
-; ASM FILE code\common\maps\mapcoords.asm :
-; 0x7B71..0x7BDE : Map coords data
-SavepointMapCoords:
-                incbin "data/battles/global/savepointmapcoords.bin"
-RaftResetMapCoords:
-                incbin "data/battles/global/raftresetmapcoords.bin"
