9b6a4124dc1c74223f40de118b7a4257b30ce12b
Wiz <the_wizard_who_says_ni@hotmail.com>
2020-06-15 23:14:21 +0200

    Implemented patch PERCENT_POISON_DAMAGE.

--- disasm/code/gameflow/battle/battlefunctions_1.asm
+++ disasm/code/gameflow/battle/battlefunctions_1.asm
@@ -941,7 +941,16 @@ HandleAfterTurnEffects:
                 andi.w  #STATUSEFFECTS_MASK_POISON,d1
                 beq.s   @UpdateStats
                 move.w  d0,((TEXT_NAME_INDEX_1-$1000000)).w
+                
+                if (PERCENT_POISON_DAMAGE>=1)
+                jsr     GetMaxHP
+                mulu.w  #PERCENT_POISON_DAMAGE,d1
+                divu.w  #100,d1
+                andi.l  #$FFFF,d1
+                else
                 moveq   #POISON_DAMAGE,d1 ; constant poison damage
+                endif
+                
                 move.l  d1,((TEXT_NUMBER-$1000000)).w
                 txt     $133            ; "{CLEAR}{NAME} gets damaged{N}by {#} because of the poison.{D3}"
                 jsr     j_DecreaseCurrentHP
