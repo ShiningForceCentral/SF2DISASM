09b7991ac0bf63733a7afe99a9746274908aa8a4
Wiz <the_wizard_who_says_ni@hotmail.com>
2020-06-16 00:11:40 +0200

    Removed patch CAPITALIZED_CHARACTER_NAMES.

--- disasm/layout/sf2-02-0x008000-0x010000.asm
+++ disasm/layout/sf2-02-0x008000-0x010000.asm
@@ -22,13 +22,6 @@
                 include "code\gameflow\battle\battlefieldengine_2.asm"    ; Battlefield engine
                 include "code\gameflow\battle\aiengine.asm"    ; AI engine
                 include "data\stats\spells\spellnames.asm"    ; Spell names
-
-                if (CAPITALIZED_CHARACTER_NAMES=1)
-                include "data\stats\allies\allynames-capitalized.asm"
-                include "data\stats\enemies\enemynames-capitalized.asm"
-                else
                 include "data\stats\allies\allynames.asm"    ; Ally names
                 include "data\stats\enemies\enemynames.asm"    ; Enemy names
-                endif
-
 algn_FF87:      align $8000
