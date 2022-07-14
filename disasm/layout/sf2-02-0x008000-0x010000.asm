fa398aa33b2c1d7359412178012311c33c308144
xenometal <2898219+xenometal@users.noreply.github.com>
2022-07-14 02:50:54 -0400

    Standardize implementation

--- disasm/layout/sf2-02-0x008000-0x010000.asm
+++ disasm/layout/sf2-02-0x008000-0x010000.asm
@@ -68,13 +68,11 @@
                 include "data\battles\global\krakenmovecosts.asm"    ; Kraken move costs table
                 
                 include "data\stats\spells\spellnames.asm"    ; Spell names
-
-                if (CAPITALIZED_CHARACTER_NAMES=1)
-                include "data\stats\allies\allynames-capitalized.asm"
-                include "data\stats\enemies\enemynames-capitalized.asm"
+                if (STANDARD_BUILD&CAPITALIZED_CHARACTER_NAMES=1)
+                    include "data\stats\allies\allynames-capitalized.asm"
+                    include "data\stats\enemies\enemynames-capitalized.asm"
                 else
-                include "data\stats\allies\allynames.asm"    ; Ally names
-                include "data\stats\enemies\enemynames.asm"    ; Enemy names
+                    include "data\stats\allies\allynames.asm"    ; Ally names
+                    include "data\stats\enemies\enemynames.asm"    ; Enemy names
                 endif
-
                 align $10000
--- disasm/sf2patches.asm
+++ disasm/sf2patches.asm
@@ -1,1 +1,4 @@
-CAPITALIZED_CHARACTER_NAMES: equ 0      ; 0 = OFF, 1 = ON
+
+; 0 = OFF, 1 = ON
+
+CAPITALIZED_CHARACTER_NAMES: equ 0      ; Capitalize allies and enemies names, as well as change "JAR" and the Chess Army's "DARK BISHOP" to "Jaro" and "Bishop".
