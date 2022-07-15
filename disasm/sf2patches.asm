8e0ea4341eeb9c618c0c8e6e87f9d9e85ee25f5c
xenometal <2898219+xenometal@users.noreply.github.com>
2022-07-15 00:51:06 -0400

    Standardize implementation

--- disasm/sf2patches.asm
+++ disasm/sf2patches.asm
@@ -1,1 +1,4 @@
-BUGFIX_MOVEMENT_GLITCH: equ 1                   ; 0 = OFF, 1 = ON.
+
+; 0 = OFF, 1 = ON
+
+FIX_MOVEMENT_GLITCH: equ 1                   ; The movement glitch is used in battles to reach places which are out of the controlled character's moving boundaries.
