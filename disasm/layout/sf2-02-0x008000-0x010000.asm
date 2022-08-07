3845498148b24fce58418921f4a8c7df79346405
xenometal <2898219+xenometal@users.noreply.github.com>
2022-08-07 03:22:50 -0400

    Implement patch SEND_DESTROYED_ITEMS_TO_DEALS

--- disasm/layout/sf2-02-0x008000-0x010000.asm
+++ disasm/layout/sf2-02-0x008000-0x010000.asm
@@ -37,8 +37,12 @@
                 include "code\gameflow\battle\battleactions\castspell.asm"    ; Write Battlescene Script : Cast Spell
                 include "code\gameflow\battle\battleactions\calculatespelldamage.asm"    ; Calculate Spell Damage function
                 include "code\gameflow\battle\battleactions\useitem.asm"    ; Write Battlescene Script : Use Item
+            if (STANDARD_BUILD=1)
+                include "code\gameflow\battle\battleactions\breakuseditem-standard.asm"
+            else
                 include "code\gameflow\battle\battleactions\nullsub_BBE4.asm"    ; Unused nullsub
                 include "code\gameflow\battle\battleactions\breakuseditem.asm"    ; Write Battlescene Script : Break Used Item
+            endif
                 include "data\stats\items\itembreakmessages.asm"    ; Item break messages
                 include "code\gameflow\battle\battleactions\writebattlescenecommanddropenemyitem.asm"    ; Write Battlescene Command : Drop Enemy Item function
                 include "data\battles\global\enemyitemdrops.asm"    ; Enemy item drops
---
+++ disasm/sf2patches.asm
@@ -0,0 +1,4 @@
+
+; 0 = OFF, 1 = ON
+
+SEND_DESTROYED_ITEMS_TO_DEALS: equ 1    ; Add item that was destroyed upon usage to shop deals if rare and if not a consumable.
