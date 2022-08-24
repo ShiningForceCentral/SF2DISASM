adb1d40fc51a8d3c2629c024e0c2cc6984f0cf24
xenometal <2898219+xenometal@users.noreply.github.com>
2022-08-23 23:12:06 -0400

    Implement patch FIX_DWARVEN_MINER_VOICE

---
+++ disasm/sf2patches.asm
@@ -0,0 +1,5 @@
+
+; 0 = OFF, 1 = ON
+
+; Fixes
+FIX_DWARVEN_MINER_VOICE:    equ 1       ; A cutscene has a fairy NPC concluding her dialogue, and when the dwarf delivers his first line, it uses the same female NPC speech sound effect until his follow-up dialogue line.
