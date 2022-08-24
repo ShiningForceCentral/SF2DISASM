4b2167f847f24abe704197d6ce6b3ef229daa112
xenometal <2898219+xenometal@users.noreply.github.com>
2022-08-24 05:08:13 -0400

    Implement patch SEND_DROPPED_ITEMS_TO_CARAVAN

---
+++ disasm/sf2patches.asm
@@ -0,0 +1,4 @@
+
+; 0 = OFF, 1 = ON
+
+SEND_DROPPED_ITEMS_TO_CARAVAN: equ 0    ; If character inventory is full, add dropped item to the Caravan instead of shop deals, regardless of rarity. If the Caravan itself is full, then drops follow the usual routine: go to deals if rare, or become lost completely if not rare.
