95f6dc2c92ae302a3ee2587c988fc89e08ad3409
pyroHedge <77116540+pyroHedge@users.noreply.github.com>
2024-01-21 00:18:41 -0600

    FIX_COUNT_DEFEATED_ENEMIES
    Fixes the glitch in the original game where the subroutine CountDefeatedEnemies checks for the number of ally slots available in battle rather than the number of monster slots. This has no effect in the original game for battles 16 and 20 because the number of ally and monster slots are identical. But it makes battle 21 (Chess) much less aggressive than originally designed.

---
+++ disasm/sf2patches.asm
@@ -0,0 +1,5 @@
+
+; 0 = OFF, 1 = ON
+
+; Fixes
+FIX_COUNT_DEFEATED_ENEMIES:         equ 1       ; Fixes the death counter that impacts "swarm AI" for battles 16, 20, 21. In vanilla, does not impact battles 16, 20, but makes 21 more aggressive.
