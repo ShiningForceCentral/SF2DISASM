aca0a53a26b359c551fed727e0f251292cf757b3
xenometal <2898219+xenometal@users.noreply.github.com>
2025-01-11 02:56:56 -0500

    Align textbank binaries

--- disasm/data/scripting/text/entries.asm
+++ disasm/data/scripting/text/entries.asm
@@ -2,23 +2,39 @@
 ; ASM FILE data\scripting\text\entries.asm :
 ; 0x2EB34..0x4201E : Textbank entries
 TextBank00:     incbin "data/scripting/text/textbank00.bin"
+                align
 TextBank01:     incbin "data/scripting/text/textbank01.bin"
+                align
 TextBank02:     incbin "data/scripting/text/textbank02.bin"
+                align
 TextBank03:     incbin "data/scripting/text/textbank03.bin"
+                align
 TextBank04:     incbin "data/scripting/text/textbank04.bin"
+                align
 TextBank05:     incbin "data/scripting/text/textbank05.bin"
+                align
 TextBank06:     incbin "data/scripting/text/textbank06.bin"
+                align
 TextBank07:     incbin "data/scripting/text/textbank07.bin"
+                align
 TextBank08:     incbin "data/scripting/text/textbank08.bin"
+                align
 TextBank09:     incbin "data/scripting/text/textbank09.bin"
+                align
 TextBank10:     incbin "data/scripting/text/textbank10.bin"
+                align
 TextBank11:     incbin "data/scripting/text/textbank11.bin"
+                align
 TextBank12:     incbin "data/scripting/text/textbank12.bin"
+                align
 TextBank13:     incbin "data/scripting/text/textbank13.bin"
+                align
 TextBank14:     incbin "data/scripting/text/textbank14.bin"
+                align
 TextBank15:     incbin "data/scripting/text/textbank15.bin"
+                align
 TextBank16:     incbin "data/scripting/text/textbank16.bin"
-                dc.b $FF                ; align to make sure we're on an even address
+                align
 pt_TextBanks:   dc.l TextBank00
                 dc.l TextBank01
                 dc.l TextBank02
