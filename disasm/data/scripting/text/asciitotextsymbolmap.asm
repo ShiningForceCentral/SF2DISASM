2a898e562a57b0c57f6aadca7bc598c09d0f3dd7
xenometal <2898219+xenometal@users.noreply.github.com>
2025-08-26 22:14:23 -0400

    Split ASCII to Variable Width Font text symbol map data table from text functions code file

---
+++ disasm/data/scripting/text/asciitotextsymbolmap.asm
@@ -0,0 +1,260 @@
+
+; ASM FILE data\scripting\text\asciitotextsymbolmap.asm :
+; 0x666E..0x676E : ASCII to Variable Width Font text symbol map table
+table_AsciiToTextSymbolMap:
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 68                 ; !
+                dc.b 1
+                dc.b 75                 ; #
+                dc.b 1
+                dc.b 76                 ; %
+                dc.b 77                 ; &
+                dc.b 72                 ; '
+                dc.b 73                 ; (
+                dc.b 74                 ; )
+                dc.b 1
+                dc.b 78                 ; +
+                dc.b 67                 ; ,
+                dc.b 65                 ; -
+                dc.b 66                 ; .
+                dc.b 79                 ; /
+                dc.b 2                  ; 0
+                dc.b 3                  ; 1
+                dc.b 4                  ; 2
+                dc.b 5                  ; 3
+                dc.b 6                  ; 4
+                dc.b 7                  ; 5
+                dc.b 8                  ; 6
+                dc.b 9                  ; 7
+                dc.b 10                 ; 8
+                dc.b 11                 ; 9
+                dc.b 80                 ; :
+                dc.b 1
+                dc.b 73                 ; (
+                dc.b 1
+                dc.b 74                 ; )
+                dc.b 69                 ; ?
+                dc.b 1
+                dc.b 12                 ; A
+                dc.b 13                 ; B
+                dc.b 14                 ; C
+                dc.b 15                 ; D
+                dc.b 16                 ; E
+                dc.b 17                 ; F
+                dc.b 18                 ; G
+                dc.b 19                 ; H
+                dc.b 20                 ; I
+                dc.b 21                 ; J
+                dc.b 22                 ; K
+                dc.b 23                 ; L
+                dc.b 24                 ; M
+                dc.b 25                 ; N
+                dc.b 26                 ; O
+                dc.b 27                 ; P
+                dc.b 28                 ; Q
+                dc.b 29                 ; R
+                dc.b 30                 ; S
+                dc.b 31                 ; T
+                dc.b 32                 ; U
+                dc.b 33                 ; V
+                dc.b 34                 ; W
+                dc.b 35                 ; X
+                dc.b 36                 ; Y
+                dc.b 37                 ; Z
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 64                 ; _
+                dc.b 1
+                dc.b 38                 ; a
+                dc.b 39                 ; b
+                dc.b 40                 ; c
+                dc.b 41                 ; d
+                dc.b 42                 ; e
+                dc.b 43                 ; f
+                dc.b 44                 ; g
+                dc.b 45                 ; h
+                dc.b 46                 ; i
+                dc.b 47                 ; j
+                dc.b 48                 ; k
+                dc.b 49                 ; l
+                dc.b 50                 ; m
+                dc.b 51                 ; n
+                dc.b 52                 ; o
+                dc.b 53                 ; p
+                dc.b 54                 ; q
+                dc.b 55                 ; r
+                dc.b 56                 ; s
+                dc.b 57                 ; t
+                dc.b 58                 ; u
+                dc.b 59                 ; v
+                dc.b 60                 ; w
+                dc.b 61                 ; x
+                dc.b 62                 ; y
+                dc.b 63                 ; z
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 64                 ; _
+                dc.b 1
+                dc.b 38                 ; a
+                dc.b 39                 ; b
+                dc.b 40                 ; c
+                dc.b 41                 ; d
+                dc.b 42                 ; e
+                dc.b 43                 ; f
+                dc.b 44                 ; g
+                dc.b 45                 ; h
+                dc.b 46                 ; i
+                dc.b 47                 ; j
+                dc.b 48                 ; k
+                dc.b 49                 ; l
+                dc.b 50                 ; m
+                dc.b 51                 ; n
+                dc.b 52                 ; o
+                dc.b 53                 ; p
+                dc.b 54                 ; q
+                dc.b 55                 ; r
+                dc.b 56                 ; s
+                dc.b 57                 ; t
+                dc.b 58                 ; u
+                dc.b 59                 ; v
+                dc.b 60                 ; w
+                dc.b 61                 ; x
+                dc.b 62                 ; y
+                dc.b 63                 ; z
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 64                 ; _
+                dc.b 1
+                dc.b 66                 ; .
+                dc.b 73                 ; (
+                dc.b 74                 ; )
+                dc.b 66                 ; .
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
+                dc.b 1
