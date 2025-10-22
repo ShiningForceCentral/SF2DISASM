32862fef72b86bed3aaa4b32bc64eeab0d82cfd4
Wiz <the_wizard_who_says_ni@hotmail.com>
2025-10-22 17:35:51 +0200

    Minor fix on shared animation label for maps 9 and 16.
    https://github.com/ShiningForceCentral/SF2DISASM/pull/64#issuecomment-3433041085

--- disasm/data/maps/entries/map16/9-animations.asm
+++ disasm/data/maps/entries/map16/9-animations.asm
@@ -1,7 +1,6 @@
 
 ; ASM FILE data\maps\entries\map16\9-animations.asm :
 ; 0xA2DD0..0xA2DE6 : 
-Map09s9_Animations:
                 mapAnimation 78, 64
                   mapAnimEntry 0, 32, $300, 20
                   mapAnimEntry 32, 32, $300, 20
